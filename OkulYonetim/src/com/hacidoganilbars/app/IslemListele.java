0966466e3198449aab5a1668727c404908712cbf
HP <HP@HP-Bilgisayar>
2017-08-21 20:29:57 +0300

    JPA Okul Odevi

--- OkulYonetim/src/com/hacidoganilbars/app/IslemListele.java
+++ OkulYonetim/src/com/hacidoganilbars/app/IslemListele.java
@@ -1,20 +1,19 @@
 package com.hacidoganilbars.app;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.hacidoganilbars.dao.DAO;
-import com.hacidoganilbars.entity.Ogrenci;
-import com.hacidoganilbars.entity.Ogretmen;
+
 
 public class IslemListele {
 
 	public static void main(String[] args) {
 		DAO dao = new DAO();
-
-		Ogrenci ogrenci = new Ogrenci();
-		dao.getOgrenci();
 		
+		dao.connection();
+		
+		dao.ogrList();
+		dao.ogrtList();
+		dao.mudList();
+	
 
 	}
 
--- OkulYonetim/src/com/hacidoganilbars/dao/DAO.java
+++ OkulYonetim/src/com/hacidoganilbars/dao/DAO.java
@@ -5,9 +5,11 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
+import com.hacidoganilbars.entity.Mudur;
 import com.hacidoganilbars.entity.Ogrenci;
-
+import com.hacidoganilbars.entity.Ogretmen;
 
 public class DAO {
 	public EntityManager em;
@@ -29,9 +31,53 @@ public class DAO {
 		em.remove(obj);
 		em.getTransaction().commit();
 	}
-	
-	public List<Ogrenci> getOgrenci() {
-	return  em.createQuery("SELECT u FROM Ogrenci u").getResultList();
-}
+
+	public List<Ogrenci> ogrList() {
+		EntityManagerFactory entityManagerFactoryObject = Persistence.createEntityManagerFactory("OkulYonetim");
+		EntityManager entityManagerObject = entityManagerFactoryObject.createEntityManager();
+		TypedQuery<Ogrenci> ogrQuery = em.createQuery("SELECT o FROM Ogrenci o", Ogrenci.class);
+		List<Ogrenci> ogrenciList = ogrQuery.getResultList();
+		System.out.println("\n---------OGRENCÝ LÝSTESÝ---------");
+		for (Ogrenci o : ogrenciList) {
+			System.out.println(o.getId() + " " + o.getIsim() + " " + o.getSoyIsim() + " " + o.getSinifi());
+		}
+		System.out.println("---------------------------------\n");
+
+		entityManagerObject.close();
+		entityManagerFactoryObject.close();
+		return null;
+	}
+
+	public List<Ogretmen> ogrtList() {
+		EntityManagerFactory entityManagerFactoryObject = Persistence.createEntityManagerFactory("OkulYonetim");
+		EntityManager entityManagerObject = entityManagerFactoryObject.createEntityManager();
+		TypedQuery<Ogretmen> ogrtQuery = em.createQuery("SELECT o FROM Ogretmen o", Ogretmen.class);
+		List<Ogretmen> ogretmenList = ogrtQuery.getResultList();
+		System.out.println("\n---------OGRETMEN LÝSTESÝ---------");
+		for (Ogretmen o : ogretmenList) {
+			System.out.println(o.getId() + " " + o.getIsim() + " " + o.getSoyIsim() + " " + o.getBrans());
+		}
+		System.out.println("---------------------------------\n");
+
+		entityManagerObject.close();
+		entityManagerFactoryObject.close();
+		return null;
+	}
+
+	public List<Mudur> mudList() {
+		EntityManagerFactory entityManagerFactoryObject = Persistence.createEntityManagerFactory("OkulYonetim");
+		EntityManager entityManagerObject = entityManagerFactoryObject.createEntityManager();
+		TypedQuery<Mudur> mudQuery = em.createQuery("SELECT m FROM Mudur m", Mudur.class);
+		List<Mudur> mudurList = mudQuery.getResultList();
+		System.out.println("\n---------MUDUR LÝSTESÝ---------");
+		for (Mudur m : mudurList) {
+			System.out.println(m.getId() + " " + m.getIsim() + " " + m.getSoyIsim());
+		}
+		System.out.println("---------------------------------\n");
+
+		entityManagerObject.close();
+		entityManagerFactoryObject.close();
+		return null;
+	}
 
 }
